7dfb78d1670546d65a458466b829501e43dcc52a
kmv-dev <kmvdev@yandex.ru>
2021-03-17 23:00:36 +0700

    live reload styles,html+

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
@@ -12,19 +12,22 @@ function liveReload() {
 }
 
 
+
 function styles() {
     return src('src/scss/style.scss')
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(concat('style.min.css'))
-        .pipe(dest('dist/css'))
+        .pipe(dest('src/css'))
         .pipe(browserSync.stream())
 }
 
 function watching() {
-    watch(['src/scss/**/*.scss'], styles)
-    watch(['src/*.html']).on('change', browserSync.reload)
+    watch(['src/scss/**/*.scss'], styles);
+    watch(['src/*.html']).on('change', browserSync.reload);
 }
 
 exports.styles = styles;
 exports.watching = watching;
 exports.liveReload = liveReload;
+
+exports.default = parallel(liveReload, watching);
